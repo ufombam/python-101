455dbb44b1c3567fd812c288b5f8076979ec8c2d
Moses Ufomba <ufombam@gmail.com>
2021-01-18 23:01:59 +0100

    progress

---
+++ classwork.py
@@ -0,0 +1,10 @@
+from sys import argv
+script, classroom = argv
+request = "> "
+
+print argv
+
+print "What food do you like? "
+Food = raw_input(request)
+
+print "You said you like %s!" % Food
--- ex11.py
+++ ex11.py
@@ -5,4 +5,5 @@ height = raw_input()
 print "How much do you weigh?",
 weight = raw_input()
 
-print "So, you're %r old, %r tall and %r heavy." % (age, height, weight)
+print "So, you're %r old, %r tall and %r heavy." % (
+    age, height, weight)
